0854882eec95309cc603a16fd108364f875c236f
Rich Samartino <rich@richsamartino.com>
2019-09-29 22:09:08 -0700

    add system configuration to show/hide button

--- Block/DownloadButton.php
+++ Block/DownloadButton.php
@@ -4,14 +4,51 @@ namespace Rich\CartCsv\Block;
 
 use Magento\Catalog\Block\ShortcutInterface;
 use Magento\Framework\View\Element\Template;
+use Rich\CartCsv\Model\Config;
 
 class DownloadButton extends Template implements ShortcutInterface
 {
     const ALIAS_ELEMENT_INDEX = 'alias';
+
+    /**
+     * @var Config
+     */
+    private $config;
+
+    public function __construct(
+        Config $config,
+        Template\Context $context,
+        array $data = []
+    )
+    {
+        parent::__construct($context, $data);
+
+        $this->config = $config;
+    }
+
 
     public function getAlias()
     {
         return $this->getData(self::ALIAS_ELEMENT_INDEX);
     }
 
+    /**
+     * @inheritdoc
+     */
+    protected function _toHtml()
+    {
+        if ($this->isActive()) {
+            return parent::_toHtml();
+        }
+
+        return '';
+    }
+
+    /**
+     * @return bool
+     */
+    public function isActive()
+    {
+        return $this->config->isDownloadButtonEnabled();
+    }
 }
---
+++ Model/Config.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Rich\CartCsv\Model;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Config
+{
+    const XML_PATH_DISPLAY_DOWNLOAD_BUTTON = 'checkout/sidebar/csvDownload';
+
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * Config constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    public function isDownloadButtonEnabled()
+    {
+        return (bool) $this->scopeConfig->getValue(self::XML_PATH_DISPLAY_DOWNLOAD_BUTTON);
+    }
+}
